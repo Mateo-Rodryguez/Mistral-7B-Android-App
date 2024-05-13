2f6a2ea058bf2ac9f826e8c07a3b67f424de10cd
Mateo Rodriguez (TMETC Plc) <mateo.rodriguez@tatamotors.com>
2024-05-13 16:27:53 +0100

    process api js

--- Website_platform/main.js
+++ Website_platform/main.js
@@ -1,13 +1,22 @@
-function postMessage(userInput){
-    fetch('http://127.0.0.1:5000'{
+function processUserInput(userInput) {
+    // processing user input
+    addChatMessage("You: " + userInput, true);
+    
+    fetch('http:localhost:5000/generate',{
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
+            'Accept': 'application/json'
         },
-        body: JSON.stringify({ prompt: userInput})
+        body: JSON.stringify({promt: userInput})
     })
     .then(response => response.json())
-    .then(data => {
-        const ul = document.querySelector('.listcontainer ul')
+    .then(data =>{
+        // Adding the bot's response to the chat
+        addChatMessage("Bot: " + data.response, false);
     })
+    .catch(error => console.error('Error:', error));
+}
+function addChatMessage(message, isUser) {
+    chatAdapter.add({message: message, isUser: isUser})
 }
